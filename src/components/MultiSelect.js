9035474724452b2b0794cdf21f59413b07cd5ff6
aniket <aniketkoli17884@gmail.com>
2025-01-23 12:37:25 +0530

    Cancel the updated code

--- src/components/MultiSelect.js
+++ src/components/MultiSelect.js
@@ -35,12 +35,9 @@ export default function MultiSelect({ rowId, selectedMultiOptions, updateSelecte
     }
 
     const updatedOptions = [...options, newOption];
-    setOptions(updatedOptions); // Update the local storage and component state with the new option
+    setOptions(updatedOptions);
     setNewOption("");
     setError("");
-
-    // Trigger re-render to show the new row
-    updateSelectedMultiOptions(rowId, selected); // Make sure the parent component is aware of the updated options
   };
 
   // Close dropdown if clicked outside
